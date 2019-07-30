db56d39651b9cd1131a225903c6f4b02691a834b
tcb1978 <mattheweldredge@gmail.com>
2019-07-29 17:28:52 -0700

    AddBtn and AddLofModal components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogmodal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -18,6 +20,8 @@ const App = () => {
       </header>
       <main>
         <div className="container">
+          <AddBtn />
+          <AddLogModal />
           <Logs />
         </div>
       </main>
