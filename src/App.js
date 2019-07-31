6dfb2e3b847b4373c8d49bef0bd7753bf1432ae7
tcb1978 <mattheweldredge@gmail.com>
2019-07-30 20:46:46 -0700

    redux tools and provider steup

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -17,22 +19,24 @@ const App = () => {
     M.AutoInit();
   }, []);
   return (
-    <Fragment>
-      <header>
-        <SearchBar />
-      </header>
-      <main>
-        <div className="container">
-          <AddBtn />
-          <AddLogModal />
-          <EditLogModal />
-          <AddTechModal />
-          <TechListModal />
-          <Logs />
-        </div>
-      </main>
-      <footer />
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <header>
+          <SearchBar />
+        </header>
+        <main>
+          <div className="container">
+            <AddBtn />
+            <AddLogModal />
+            <EditLogModal />
+            <AddTechModal />
+            <TechListModal />
+            <Logs />
+          </div>
+        </main>
+        <footer />
+      </Fragment>
+    </Provider>
   );
 };
 
